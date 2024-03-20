a7a7422bb1e770d8d8247ae06e2c6581508d3a12
Rahul Pandita <rpandita1999@outlook.com>
2024-03-20 10:19:38 +0530

    3d example

--- slicing.py
+++ slicing.py
@@ -1,17 +1,24 @@
 import numpy as np
 
 a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
-print(a.shape)
-print(a)
+# print(a.shape)
+# print(a)
 
 # Get a specific element [row, column]
-print(a[0, 6])
+# print(a[0, 6])
 
 # Get a specific row
-print(a[0, :])
+# print(a[0, :])
 
 # Get a specific column
-print(a[:, 6])
+# print(a[:, 6])
 
-a[0,6] = 700000000
-print(a)
+# a[0,6] = 700000000
+# print(a)
+
+# 3D example
+b = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(b.shape)
+print(b)
+
+print(b[1, 1, 1])
