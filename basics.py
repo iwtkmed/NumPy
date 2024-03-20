2d7a6c3a0ffebc5bb8d1161b9c2cb9520098d061
Rahul Pandita <rpandita1999@outlook.com>
2024-03-20 09:15:59 +0530

    Get item data type, item size and array total size

--- basics.py
+++ basics.py
@@ -2,20 +2,32 @@ import numpy as np
 
 a = np.array([1, 2, 3])
 
-print(a)
+# print(a)
 
 # list
-x = [[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]]
-print(x)
+# x = [[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]]
+# print(x)
 
 # numpy 2d array
 y = np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
-print(y)
+# print(y)
 
 # Get dimensions of your numpy array
-print(a.ndim)
-print(y.ndim)
+# print(a.ndim)
+# print(y.ndim)
 
 # Get shape of numpy array
-print(a.shape)
-print(y.shape)
+# print(a.shape)
+# print(y.shape)
+
+# Get Type
+print(a.dtype)
+print(y.dtype)
+
+# Get Size
+print(a.itemsize)
+print(y.itemsize)
+
+# Get total size
+print(a.nbytes)
+print(y.nbytes)
