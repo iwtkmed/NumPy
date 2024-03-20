b5daaac99291b457c3d90e9fc7ba15a25ed8f7f2
Rahul Pandita <rpandita1999@outlook.com>
2024-03-20 10:26:54 +0530

    Initialize all 0's matrix

---
+++ initialize.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# INITIALIZING DIFFERENT TYPES OF ARRAYS
+
+# All 0's matrix
+a = np.zeros((5))
+print(a)
+
+print()
+
+b = np.zeros((3, 4))
+print(b)
+
+print()
+
+c = np.zeros((4, 3, 3))
+print(c)
