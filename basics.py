c5e4d34c3ae37f63f3a30e302cee6a061c2feef8
Rahul Pandita <rpandita1999@outlook.com>
2024-03-20 09:03:00 +0530

    Create list, np 1d array and np 2d array; compare differences in output

---
+++ basics.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+
+print(a)
+
+# list
+x = [[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]]
+print(x)
+
+# numpy 2d array
+y = np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
+print(y)
