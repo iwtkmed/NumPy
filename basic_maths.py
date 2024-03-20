052a0c1dacbf331ee9bcd35ee1f3623ded0a1c7c
Rahul Pandita <rpandita1999@outlook.com>
2024-03-20 11:23:47 +0530

    Perform basic trig function calculations on every element of numpy array

--- basic_maths.py
+++ basic_maths.py
@@ -2,8 +2,16 @@ import numpy as np
 
 a = np.array([1,2,3,4,5])
 
-print(a + 2)
-print(a - 2)
-print(a * 2)
-print(a / 2)
-print(a ** 3)
+b = np.array([10,11,12,13,0])
+# print(a + 2)
+# print(a - 2)
+# print(a * 2)
+# print(a / 2)
+# print(a ** 3)
+
+# Add elements of two arrays; arrays should be of same shape
+# print(a + b)
+
+# Basic trig
+print(np.sin(a))
+print(np.cos(a))
